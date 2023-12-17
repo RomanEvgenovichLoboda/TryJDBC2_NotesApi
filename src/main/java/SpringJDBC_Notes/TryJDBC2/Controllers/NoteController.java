c95c092e58858d2e42dfa0b1ab0b5effa22403d4
RomanEvgenovichLoboda <loboda.rom@gmail.com>
2023-12-17 13:25:02 +0200

    addRoutes

--- src/main/java/SpringJDBC_Notes/TryJDBC2/Controllers/NoteController.java
+++ src/main/java/SpringJDBC_Notes/TryJDBC2/Controllers/NoteController.java
@@ -2,11 +2,14 @@ package SpringJDBC_Notes.TryJDBC2.Controllers;
 
 import SpringJDBC_Notes.TryJDBC2.Models.Note;
 import SpringJDBC_Notes.TryJDBC2.Repositories.NoteRepository;
+import org.apache.velocity.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Controller
 @RequestMapping(path = "/note")
@@ -23,5 +26,28 @@ public class NoteController {
     @GetMapping(path = "/getAll")
     public @ResponseBody Iterable<Note> getAllNotes() {
         return noteRepo.findAll();
+    }
+
+    @DeleteMapping(path = "/delleteById")
+    public @ResponseBody String deleteById(@RequestParam Integer id){
+        noteRepo.deleteById(id);
+        return "Delleted";
+    }
+
+    @PutMapping(path = "/update")
+    public @ResponseBody Note updateNote(Integer id, String newText){
+        Note note = noteRepo.findById(id).orElseThrow(() -> new ResourceNotFoundException("Note not exist with id = " + id));
+        note.setText(newText);
+        note.setDateTime(LocalDateTime.now());
+        noteRepo.save(note);
+        return note;
+    }
+
+    @PatchMapping(path = "/partialUpdate")
+    public @ResponseBody Note partialUpdate(Integer id, String newText){
+        Note note = noteRepo.findById(id).orElseThrow(() -> new ResourceNotFoundException("Note not exist with id = " + id));
+        note.setText(newText);
+        noteRepo.save(note);
+        return note;
     }
 }
--- src/main/java/SpringJDBC_Notes/TryJDBC2/Models/Note.java
+++ src/main/java/SpringJDBC_Notes/TryJDBC2/Models/Note.java
@@ -17,7 +17,7 @@ import java.time.LocalDateTime;
 @Entity // This tells Hibernate to make a table out of this class
 public class Note {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private String avtor;
